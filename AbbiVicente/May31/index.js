4e552311cc7e3c9926ffd81f3817515f33e5bcf5
Isabel Louise Vicente <isabellouiseph@gmail.com>
2023-05-31 17:15:01 +0800

    javascript basics activity

--- AbbiVicente/May31/index.js
+++ AbbiVicente/May31/index.js
@@ -2,60 +2,57 @@
 let numbers = [1, -8, 20, 7, 13];
 
 // 2. find the minimum and maximum numbers
-function findMinMax(numbers) {
-	let max = numbers[0];
-	let min = numbers[0];
+function findMinMax(arr) {
+	let max = arr[0];
+	let min = arr[0];
 
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > max) {
-			max = numbers[i];
-		} else if (numbers[i] < min) {
-			min = numbers[i];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		} else if (arr[i] < min) {
+			min = arr[i];
 		}
 	}
-	console.log(`Maximum number is ${max}`);
-	console.log(`Minimum number is ${min}`);
+	console.log(`${max}`);
+	console.log(`${min}`);
 }
 findMinMax(numbers);
 
 // 3. tell whether the number is even or odd
-function printEvenOdd(numbers) {
-	let even = numbers[0];
-	let odd = numbers[0];
+function printEvenOdd(arr) {
+	let even = arr[0];
+	let odd = arr[0];
 
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] % 2 === 0) {
-			console.log(`${numbers[i]} is even`);
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			console.log(`${arr[i]} is even`);
 		} else {
-			console.log(`${numbers[i]} is odd`);
+			console.log(`${arr[i]} is odd`);
 		}
 	}
 }
 printEvenOdd(numbers);
-
-let x = 13;
-let y = 7;
 
 // 4. add two numbers
 function Add(x, y) {
 	let sum = x + y;
 	console.log(`The sum of ${x} and ${y} is ${sum}`);
 }
-Add(x, y);
+Add(7, 13);
 
 // 5. subtract two numbers
 function Subtract(x, y) {
 	let difference = x - y;
 	console.log(`The difference of ${x} and ${y} is ${difference}`);
 }
-Subtract(x, y);
+Subtract(20, 13);
 
 // 6. multiply two numbers
 function Multiply(x, y) {
 	let product = x * y;
 	console.log(`The product of ${x} and ${y} is ${product}`);
 }
-Multiply(x, y);
+Multiply(7, 13);
 
 // 7. divide two numbers
 function Divide(x, y) {
@@ -63,4 +60,4 @@ function Divide(x, y) {
 	let remainder = x % y;
 	console.log(`The quotient of ${x} and ${y} is ${quotient} with a remainder of ${remainder}`);
 }
-Divide(x, y);
+Divide(20, 13);
