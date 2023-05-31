9e91a659087b035708ea181a9c088a34f8786d55
Whistner Rojas <whistner.rojas.fg@gmail.com>
2023-05-31 18:25:31 +0800

    added a remainder output

--- WhistnerRojas/May31-2023/activity.js
+++ WhistnerRojas/May31-2023/activity.js
@@ -1,79 +1,84 @@
-// Exercise: JavaScript Basics
-// 1. Declare a variable named numbers and assign it an array of at least 5 different integer values. The numbers can be any integers you choose
-// 2. Write a function named findMinMax. This function should take an array of numbers as a parameter and should print the minimum and maximum numbers from the array. The function does not need to return anything.
-// 3. Write a function named printEvenOdd. This function should take an array of numbers as a parameter and should print whether each number is even or odd.
-// 4. Write a function named Add. This function should take 2 numbers as a parameter and should print the sum of the two numbers.
-// 5. Write a function named Subtract. This function should take 2 numbers as a parameter and should print the difference of the two numbers.
-// 6. Write a function named Multiply. This function should take 2 numbers as a parameter and should print the product of the two numbers.
-// 7. Write a function named Divide. This function should take 2 numbers as a parameter and should print the quotient of the two numbers and it's remainder if there is a remainder.
+// // Exercise: JavaScript Basics
+// // 1. Declare a variable named numbers and assign it an array of at least 5 different integer values. The numbers can be any integers you choose
+// // 2. Write a function named findMinMax. This function should take an array of numbers as a parameter and should print the minimum and maximum numbers from the array. The function does not need to return anything.
+// // 3. Write a function named printEvenOdd. This function should take an array of numbers as a parameter and should print whether each number is even or odd.
+// // 4. Write a function named Add. This function should take 2 numbers as a parameter and should print the sum of the two numbers.
+// // 5. Write a function named Subtract. This function should take 2 numbers as a parameter and should print the difference of the two numbers.
+// // 6. Write a function named Multiply. This function should take 2 numbers as a parameter and should print the product of the two numbers.
+// // 7. Write a function named Divide. This function should take 2 numbers as a parameter and should print the quotient of the two numbers and it's remainder if there is a remainder.
 
 
-//1.
+// //1.
 
-let numbers = [20,0,7,8,10]
+// let numbers = [20,0,7,8,10]
 
-//2.
+// //2.
 
-function findMinMax(arr) {
-    let min = arr[0]
-    let max = arr[0]
+// function findMinMax(arr) {
+//     let min = arr[0]
+//     let max = arr[0]
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i]
-        } else if (arr[i] > max) {
-            max = arr[i]
-        }
-    }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             min = arr[i]
+//         } else if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
 
-    console.log(`Minimum number: ${min}`)
-    console.log(`Maximum number: ${max}`)
-}
+//     console.log(`Minimum number: ${min}`)
+//     console.log(`Maximum number: ${max}`)
+// }
 
-findMinMax(numbers);
+// findMinMax(numbers);
 
-//3.
+// //3.
 
-function printEvenOdd(arr){
-    for (let i = 0; i < arr.length; i++) {
-        if ((arr[i] % 2) === 0) {
-            console.log(arr[i], "is an even number.")
-        }else{
-            console.log(arr[i], "is an odd number.")
-        }
-    }
-}
+// function printEvenOdd(arr){
+//     for (let i = 0; i < arr.length; i++) {
+//         if ((arr[i] % 2) === 0) {
+//             console.log(arr[i], "is an even number.")
+//         }else{
+//             console.log(arr[i], "is an odd number.")
+//         }
+//     }
+// }
 
-printEvenOdd(numbers)
+// printEvenOdd(numbers)
 
-//4.
+// //4.
 
-function Add(num1, num2){
-    console.log(`${num1} + ${num2} =`, num1 + num2)
-}
+// function Add(num1, num2){
+//     console.log(`${num1} + ${num2} =`, num1 + num2)
+// }
 
-Add(1,2)
+// Add(1,2)
 
-//5.
+// //5.
 
-function Subtract(num1, num2){
-    console.log(`${num1} - ${num2} =`, num1 - num2)
-}
+// function Subtract(num1, num2){
+//     console.log(`${num1} - ${num2} =`, num1 - num2)
+// }
 
-Subtract(1,2)
+// Subtract(1,2)
 
-//6.
+// //6.
 
-function Multiply(num1, num2){
-    console.log(`${num1} x ${num2} =`, num1 * num2)
-}
+// function Multiply(num1, num2){
+//     console.log(`${num1} x ${num2} =`, num1 * num2)
+// }
 
-Multiply(5,2)
+// Multiply(5,2)
 
-//7.
+// //7.
 
 function Divide(num1, num2){
-    console.log(`${num1} / ${num2} =`, num1 / num2)
+
+    if((num1 / num2) % 2 === 0 ){
+        console.log(`${num1} / ${num2} =`, num1 / num2)
+    }else{
+        console.log(`${num1} / ${num2} =`, num1 / num2, `with a reminder of: ${(num1 / num2) % 1}`)
+    }
 }
 
-Divide(10,5)
+Divide(71,5)
